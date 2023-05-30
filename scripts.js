e13ace8080e5867b349c77f0b8bcc1ac58f4f09a
DrBecky22 <101670044+DrBecky22@users.noreply.github.com>
2023-05-30 01:08:13 -0500

    FINALLY LINKED!

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
-colorChange() {
+
+function colorChange() {
+    alert("you made it to ColorChange!")
     document.getElementById('pageTitle').style.color='red';
+    return false;
 }
 
+function dateBtn() {
+    alert("you are at the date button");
+    document.getElementById('demo').innerHTML = "<p> hello </p>";
+    // document.getElementById('demo').innerHTML = Date();
+    return false;
+}
