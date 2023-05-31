a941c9305e41e07101e733bb34125b2ac240830c
DrBecky22 <101670044+DrBecky22@users.noreply.github.com>
2023-05-30 22:13:39 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,7 +7,24 @@ function colorChange() {
 
 function dateBtn() {
     alert("you are at the date button");
-    document.getElementById('demo').innerHTML = "<p> hello </p>";
-    // document.getElementById('demo').innerHTML = Date();
-    return false;
+    // document.getElementById('dateTime').innerHTML = "<p> hello </p>";
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    days = ['Sunday','Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    dateData = new Date();
+    currentDay = dateData.getDay();
+    document.getElementById('day').textContent = days[currentDay];
+currentMonth = dateData.getMonth();
+currentDate = dateData.getDate();
+currentYear = dateData.getFullYear();
+document.getElementById('month-date-year').textContent = `$(months[currentMonth] $(currentDate)`;
+
+    // const date = getDate()
+    // document.getElementById('dateTime').innerHTML.date;
+    // return false;
+}
+
+function snuffyStyleScared() {
+    alert('confused button!');
+    document.getElementById('snuffyPicStart');
+    img.src = 'elephantScared.png';
 }
